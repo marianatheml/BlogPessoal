111d3bf7fefb9f57e8ef09474e32c3a624d40891
Mariana Theml <marianatheml@gmail.com>
2021-12-09 01:12:25 -0300

    Modified BasicSecurityConfig

--- src/main/java/org/generation/blogPessoal/security/BasicSecurityConfig.java
+++ src/main/java/org/generation/blogPessoal/security/BasicSecurityConfig.java
@@ -2,7 +2,6 @@ package org.generation.blogPessoal.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,7 +26,7 @@ public class BasicSecurityConfig extends  WebSecurityConfigurerAdapter {
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+		return new BCryptPasswordEncoder();		
 	}
 	
 	@Override
